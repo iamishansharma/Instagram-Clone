47c3f1fb10fe875b603e2a9e5832ff5fa664e41b
Ishan Sharma <sharmaishan747@gmail.com>
2021-08-25 21:42:59 +0530

    Initial Commit

---
+++ Instagram-Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram-Clone
+//
+//  Created by [Work] Ishan Sharma on 25/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
